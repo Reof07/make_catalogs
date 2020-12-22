0b26db5f6a4ef6f51333ab48d1515d06d1373870
Ramon Ojeda <reof1922@gmail.com>
2020-12-22 10:49:04 -0430

    change in settings prod

--- catalogs/settings/prod.py
+++ catalogs/settings/prod.py
@@ -3,9 +3,18 @@ from .base import env
 
 DEBUG=False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
-MIDDLEWARE = ('whitenoise.middleware.WhiteNoiseMiddleware',)
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 
 # Simplified static file serving.
